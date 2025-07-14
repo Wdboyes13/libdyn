30e3f1fcbdc3f66a5f772144e8456bd4ea1e9734
Wdboyes13 <willdev2025@outlook.com>
2025-07-14 08:30:45 -0700

    Updated Mon 14 Jul 2025 08:30:45 PDT

--- dynget.h
+++ dynget.h
@@ -1,20 +1,2 @@
-/*
-A C Library for tools using Dynamic Memory Allocation
-Copyright (C) 2025  Wdboyes13
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 char* getstr(const char* prompt);
